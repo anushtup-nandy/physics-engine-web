057daff463ac9c1e2773d0461ecf812932fd07a3
anushtup <110029497+anushtup-nandy@users.noreply.github.com>
2024-07-20 14:15:31 +0530

    added dat.gui and images

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Goal is to make this into a plug and play website avoiding most if not all codin
 - [x] menu to spawn blocks and balls
 - [x] simple grid like plane 
 - [x] collision detection
-- [ ] Bouncing effect when objects are spawned
+- [x] Bouncing effect when objects are spawned
 - [x] being able to move camera around
 - [ ] images to show block and ball
 - [x] Drag around objects
@@ -15,7 +15,7 @@ Goal is to make this into a plug and play website avoiding most if not all codin
 - [ ] being able to spawn more complex objects (for example rc car)
 
 ## How to use:
- *`git clone <ssh or https>` to get the repo locally
+ * `git clone <ssh or https>` to get the repo locally
  * make changes
  * Then simply add-commit and push!
 
--- main_eng.js
+++ main_eng.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/Addons.js';
 import { DragControls } from 'three/DragControls';
-// import * as dat from 'dat.gui';
+import {createBall, createBlock} from '/src/rigidbodies.js';
+import * as dat from 'dat.gui';
 
 let physicsWorld, scene, camera, renderer, clock, gridHelper, axeshelper;
 let rigidBodies = [], tmpTrans = null;
@@ -9,6 +10,7 @@ let orbitControls, raycaster, mouse, moveMouse;
 let isDragging = false;
 let dragControls;
 let draggableObjects = [];
+let gui;
 
 Ammo().then(start);
 
@@ -17,6 +19,7 @@ function start() {
     setupPhysicsWorld();
     setupGraphics();
     setupEventHandlers();
+    setupGUI();
     renderFrame();
 }
 
@@ -93,8 +96,10 @@ function setupGraphics() {
 }
 
 function setupEventHandlers() {
-    document.getElementById('addBall').addEventListener('click', createBall);
-    document.getElementById('addBlock').addEventListener('click', createBlock);
+    // document.getElementById('addBall').addEventListener('click', createBall);
+    // document.getElementById('addBlock').addEventListener('click', createBlock);
+    document.getElementById('addBall').addEventListener('click', () => createBall(physicsWorld, draggableObjects, rigidBodies, scene));
+    document.getElementById('addBlock').addEventListener('click', () => createBlock(physicsWorld, draggableObjects, rigidBodies, scene));
     window.addEventListener('resize', onWindowResize);
 }
 
@@ -127,82 +132,6 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function createBall() {
-    let pos = { x: 0, y: 10, z: 0 };
-    let radius = 1.5;
-    let quat = { x: 0, y: 0, z: 0, w: 1 };
-    let mass = 1;
-
-    let ball = new THREE.Mesh(new THREE.SphereGeometry(radius), new THREE.MeshPhongMaterial({ color: 0x800080 }));
-    ball.position.set(pos.x, pos.y, pos.z);
-    ball.castShadow = true;
-    ball.receiveShadow = true;
-    ball.userData.draggable = true;
-    ball.userData.tag = "BALL";
-    scene.add(ball);
-    draggableObjects.push(ball);
-
-    let transform = new Ammo.btTransform();
-    transform.setIdentity();
-    transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
-    transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
-    let motionState = new Ammo.btDefaultMotionState(transform);
-
-    let colShape = new Ammo.btSphereShape(radius);
-    colShape.setMargin(0.05);
-
-    let localInertia = new Ammo.btVector3(0, 0, 0);
-    colShape.calculateLocalInertia(mass, localInertia);
-
-    let rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, colShape, localInertia);
-    let body = new Ammo.btRigidBody(rbInfo);
-    body.setFriction(4);
-    body.setRollingFriction(10);
-    physicsWorld.addRigidBody(body);
-    rigidBodies.push({ threeObject: ball, ammoBody: body });
-
-    ball.userData.physicsBody = body;
-    body.threeObject = ball;
-}
-
-function createBlock() {
-    let pos = { x: 0, y: 10, z: 0 };
-    let scale = { x: 2, y: 2, z: 2 };
-    let quat = { x: 0, y: 0, z: 0, w: 1 };
-    let mass = 2;
-
-    let block = new THREE.Mesh(new THREE.BoxGeometry(scale.x, scale.y, scale.z), new THREE.MeshPhongMaterial({ color: 0x00ff00 }));
-    block.position.set(pos.x, pos.y, pos.z);
-    block.castShadow = true;
-    block.receiveShadow = true;
-    block.userData.draggable = true;
-    block.userData.tag = "BLOCK";
-    scene.add(block);
-    draggableObjects.push(block);
-
-    let transform = new Ammo.btTransform();
-    transform.setIdentity();
-    transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
-    transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
-    let motionState = new Ammo.btDefaultMotionState(transform);
-
-    let colShape = new Ammo.btBoxShape(new Ammo.btVector3(scale.x * 0.5, scale.y * 0.5, scale.z * 0.5));
-    colShape.setMargin(0.05);
-
-    let localInertia = new Ammo.btVector3(0, 0, 0);
-    colShape.calculateLocalInertia(mass, localInertia);
-
-    let rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, colShape, localInertia);
-    let body = new Ammo.btRigidBody(rbInfo);
-    body.setFriction(4);
-    body.setRollingFriction(10);
-    physicsWorld.addRigidBody(body);
-    rigidBodies.push({ threeObject: block, ammoBody: body });
-
-    block.userData.physicsBody = body;
-    body.threeObject = block;
 }
 
 function updatePhysics(deltaTime) {
@@ -242,6 +171,61 @@ function setupCollisionDetection() {
             }
         }
     }
+}
+
+function setupGUI() {
+    gui = new dat.GUI();
+
+    let ballFolder = gui.addFolder('Ball');
+    ballFolder.add({ add: () => createBall(physicsWorld, draggableObjects, rigidBodies, scene) }, 'add').name('Add Ball');
+
+    let blockFolder = gui.addFolder('Block');
+    blockFolder.add({ add: () => createBlock(physicsWorld, draggableObjects, rigidBodies, scene) }, 'add').name('Add Block');
+
+    ballFolder.open();
+    blockFolder.open();
+
+    // Add properties to edit ball
+    let ballParams = {
+        radius: 1.5,
+        color: '#800080'
+    };
+
+    ballFolder.add(ballParams, 'radius', 0.5, 5).onChange((value) => {
+        // Update the ball radius logic here
+        ballParams.radius = value;
+    });
+
+    ballFolder.addColor(ballParams, 'color').onChange((value) => {
+        // Update the ball color logic here
+        ballParams.color = value;
+    });
+
+    ballFolder.open();
+
+    // Add properties to edit block
+    let blockParams = {
+        width: 2,
+        height: 2,
+        depth: 2,
+        color: '#00ff00'
+    };
+
+    blockFolder.add(blockParams, 'width', 0.5, 5).onChange((value) => {
+        // Update the block width logic here
+    });
+
+    blockFolder.add(blockParams, 'height', 0.5, 5).onChange((value) => {
+        // Update the block height logic here
+    });
+
+    blockFolder.add(blockParams, 'depth', 0.5, 5).onChange((value) => {
+        // Update the block depth logic here
+    });
+
+    blockFolder.addColor(blockParams, 'color').onChange((value) => {
+        // Update the block color logic here
+    });
 }
 
 function renderFrame() {
